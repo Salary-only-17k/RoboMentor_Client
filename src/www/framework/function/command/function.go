4729d820546f9e3d67ada547ca920e6acbf8948e
RobotMentors <mentors@RobotMentorsdeMac-mini.local>
2020-04-11 23:10:40 +0800

    up

---
+++ src/www/framework/function/command/function.go
@@ -0,0 +1,13 @@
+package commandFunction
+
+import (
+	"os/exec"
+)
+
+func Shell(command string) (string, error){
+
+	run := exec.Command("/bin/bash", "-c", command)
+	bytes, err := run.Output()
+
+	return string(bytes), err
+}
