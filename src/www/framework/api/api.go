b595d32f51a784ced792433e3ad9acbae2fff6fa
RobotMentors <mentors@RobotMentorsdeMac-mini.local>
2020-04-12 12:50:26 +0800

    up

--- src/www/framework/api/api.go
+++ src/www/framework/api/api.go
@@ -6,6 +6,7 @@ import (
 	"github.com/shirou/gopsutil/cpu"
 	"github.com/shirou/gopsutil/mem"
 	"io/ioutil"
+	"log"
 	"net"
 	"os"
 	"runtime"
@@ -231,6 +232,8 @@ func GetHomeSkillBuild(c *gin.Context){
 			CommonService.Error(c, 10000, "技能编译失败，请求重新尝试", CommonService.EmptyData{})
 			return
 		}
+
+		log.Println("\033[31m[Error]\033[0m", shell)
 
 		if strings.Contains(shell, ":") == true {
 			CommonService.Error(c, 10000, "技能编译失败，请求重新尝试", CommonService.EmptyData{})
