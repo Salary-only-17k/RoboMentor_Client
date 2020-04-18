ccb6795cfa860ab3917ceeec4cea083b3cc3adae
RobotMentors <mentors@RobotMentorsdeMac-mini.local>
2020-04-18 19:58:11 +0800

    up

--- src/www/application/robot.go
+++ src/www/application/robot.go
@@ -25,7 +25,7 @@ func InitRobot() Robot.Interface {
 // @Title  机器人启动主程序
 // @Description	用户可以在这里设计机器人的各种程序
 func (robot *Base) OnStart() {
-	cameraDriver.StartDevice("/dev/video0")
+	cameraDriver.StartDevice("/dev/video1")
 }
 
 // @Title  机器人停止、意外停止时触发
