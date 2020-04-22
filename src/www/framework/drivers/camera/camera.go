2ab9da0a182aff4f58a47d0690533d987dc5991a
RobotMentors <mentors@RobotMentorsdeMac-mini.local>
2020-04-22 21:04:17 +0800

    up

--- src/www/framework/drivers/camera/camera.go
+++ src/www/framework/drivers/camera/camera.go
@@ -47,7 +47,7 @@ func StartDevice(Port string, Status bool) (*Driver, error) {
 
 						if Status {
 							SocketService.RobotSocketClientSend("camera_message", c.ReadImage)
-							time.Sleep(120 * time.Millisecond)
+							time.Sleep(20 * time.Millisecond)
 						}
 					}
 			}
