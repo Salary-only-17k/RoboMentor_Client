8f727c8644d859a078f467516a0fe064f295258f
RobotMentors <mentors@RobotMentorsdeMac-mini.local>
2020-05-28 00:21:09 +0800

    up

--- src/www/framework/platform/servo/servo.go
+++ src/www/framework/platform/servo/servo.go
@@ -170,7 +170,7 @@ func SitDownAction() {
 
 	SetMotion()
 
-	time.Sleep(40 * time.Millisecond)
+	time.Sleep(50 * time.Millisecond)
 
 	Servo.Action = action2
 
@@ -207,7 +207,7 @@ func StandUpAction() {
 
 	SetMotion()
 
-	time.Sleep(40 * time.Millisecond)
+	time.Sleep(50 * time.Millisecond)
 
 	Servo.Action = action2
 
