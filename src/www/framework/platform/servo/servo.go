ccc019114e75ef2ee3cc93e222f427f1eec9d6c6
RobotMentors <mentors@RobotMentorsdeMac-mini.local>
2020-05-28 01:14:32 +0800

    up

--- src/www/framework/platform/servo/servo.go
+++ src/www/framework/platform/servo/servo.go
@@ -213,3 +213,95 @@ func StandUpAction() {
 
 	SetMotion()
 }
+
+func ForwardAction() {
+	var action1 = []ActionItem{
+		{Channel:1, Id:4, Time:0, Angle:600},
+		{Channel:1, Id:5, Time:0, Angle:220},
+		{Channel:1, Id:6, Time:0, Angle:800},
+		{Channel:2, Id:1, Time:0, Angle:450},
+		{Channel:2, Id:2, Time:0, Angle:220},
+		{Channel:2, Id:3, Time:0, Angle:800},
+		{Channel:2, Id:1, Time:0, Angle:400},
+		{Channel:2, Id:2, Time:0, Angle:220},
+		{Channel:2, Id:3, Time:0, Angle:800},
+	}
+
+	var action2 = []ActionItem{
+		{Channel:1, Id:4, Time:0, Angle:600},
+		{Channel:1, Id:5, Time:0, Angle:320},
+		{Channel:1, Id:6, Time:0, Angle:850},
+		{Channel:2, Id:1, Time:0, Angle:450},
+		{Channel:2, Id:2, Time:0, Angle:320},
+		{Channel:2, Id:3, Time:0, Angle:850},
+		{Channel:2, Id:1, Time:0, Angle:400},
+		{Channel:2, Id:2, Time:0, Angle:320},
+		{Channel:2, Id:3, Time:0, Angle:850},
+
+		{Channel:1, Id:1, Time:10, Angle:550},
+		{Channel:1, Id:2, Time:10, Angle:220},
+		{Channel:1, Id:3, Time:10, Angle:800},
+		{Channel:1, Id:7, Time:10, Angle:600},
+		{Channel:1, Id:8, Time:10, Angle:220},
+		{Channel:1, Id:9, Time:10, Angle:800},
+		{Channel:2, Id:4, Time:10, Angle:400},
+		{Channel:2, Id:5, Time:10, Angle:220},
+		{Channel:2, Id:6, Time:10, Angle:800},
+	}
+
+	var action3 = []ActionItem{
+		{Channel:1, Id:4, Time:0, Angle:500},
+		{Channel:1, Id:5, Time:0, Angle:320},
+		{Channel:1, Id:6, Time:0, Angle:850},
+		{Channel:2, Id:1, Time:0, Angle:500},
+		{Channel:2, Id:2, Time:0, Angle:320},
+		{Channel:2, Id:3, Time:0, Angle:850},
+		{Channel:2, Id:1, Time:0, Angle:500},
+		{Channel:2, Id:2, Time:0, Angle:320},
+		{Channel:2, Id:3, Time:0, Angle:850},
+
+		{Channel:1, Id:1, Time:10, Angle:500},
+		{Channel:1, Id:2, Time:10, Angle:320},
+		{Channel:1, Id:3, Time:10, Angle:850},
+		{Channel:1, Id:7, Time:10, Angle:500},
+		{Channel:1, Id:8, Time:10, Angle:320},
+		{Channel:1, Id:9, Time:10, Angle:850},
+		{Channel:2, Id:4, Time:10, Angle:500},
+		{Channel:2, Id:5, Time:10, Angle:320},
+		{Channel:2, Id:6, Time:10, Angle:850},
+	}
+
+	var action4 = []ActionItem{
+		{Channel:1, Id:1, Time:10, Angle:550},
+		{Channel:1, Id:2, Time:10, Angle:320},
+		{Channel:1, Id:3, Time:10, Angle:850},
+		{Channel:1, Id:7, Time:10, Angle:600},
+		{Channel:1, Id:8, Time:10, Angle:320},
+		{Channel:1, Id:9, Time:10, Angle:850},
+		{Channel:2, Id:4, Time:10, Angle:400},
+		{Channel:2, Id:5, Time:10, Angle:320},
+		{Channel:2, Id:6, Time:10, Angle:850},
+	}
+
+	Servo.Action = action1
+
+	SetMotion()
+
+	time.Sleep(200 * time.Millisecond)
+
+	Servo.Action = action2
+
+	SetMotion()
+
+	time.Sleep(200 * time.Millisecond)
+
+	Servo.Action = action3
+
+	SetMotion()
+
+	time.Sleep(200 * time.Millisecond)
+
+	Servo.Action = action4
+
+	SetMotion()
+}
