9f4f851a2c0bf895c15e1b464f828f230fcb58f3
RobotMentors <mentors@RobotMentorsdeMac-mini.local>
2020-05-28 16:51:38 +0800

    up

--- src/www/framework/platform/servo/servo.go
+++ src/www/framework/platform/servo/servo.go
@@ -349,6 +349,30 @@ func ForwardAction() {
 		{Channel:2, Id:9, Time:-1, Angle:900},
 	}
 
+	var action6t1 = []ActionItem{
+		{Channel:1, Id:1, Time:-1, Angle:500},
+		{Channel:1, Id:2, Time:-1, Angle:320},
+		{Channel:1, Id:3, Time:-1, Angle:900},
+		{Channel:1, Id:4, Time:-1, Angle:650},
+		{Channel:1, Id:5, Time:-1, Angle:200},
+		{Channel:1, Id:6, Time:-1, Angle:900},
+		{Channel:1, Id:7, Time:-1, Angle:500},
+		{Channel:1, Id:8, Time:-1, Angle:320},
+		{Channel:1, Id:9, Time:-1, Angle:900},
+	}
+
+	var action6t2 = []ActionItem{
+		{Channel:2, Id:1, Time:-1, Angle:350},
+		{Channel:2, Id:2, Time:-1, Angle:200},
+		{Channel:2, Id:3, Time:-1, Angle:900},
+		{Channel:2, Id:4, Time:-1, Angle:500},
+		{Channel:2, Id:5, Time:-1, Angle:320},
+		{Channel:2, Id:6, Time:-1, Angle:900},
+		{Channel:2, Id:7, Time:-1, Angle:350},
+		{Channel:2, Id:8, Time:-1, Angle:200},
+		{Channel:2, Id:9, Time:-1, Angle:900},
+	}
+
 	Servo.Action = action1t1
 
 	SetMotion()
@@ -404,6 +428,18 @@ func ForwardAction() {
 	time.Sleep(60 * time.Millisecond)
 
 	Servo.Action = action5t2
+
+	SetMotion()
+
+	time.Sleep(60 * time.Millisecond)
+
+	Servo.Action = action6t1
+
+	SetMotion()
+
+	time.Sleep(60 * time.Millisecond)
+
+	Servo.Action = action6t2
 
 	SetMotion()
 }
