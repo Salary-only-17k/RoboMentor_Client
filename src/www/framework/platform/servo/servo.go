71ea880696b40827b7359ec43435d5aec9fa85e7
RobotMentors <mentors@RobotMentorsdeMac-mini.local>
2020-05-28 10:17:23 +0800

    up

--- src/www/framework/platform/servo/servo.go
+++ src/www/framework/platform/servo/servo.go
@@ -229,9 +229,9 @@ func StandUpAction() {
 
 func ForwardAction() {
 	var action1 = []ActionItem{
-		{Channel:2, Id:3, Time:-1, Angle:150},
-		{Channel:2, Id:9, Time:-1, Angle:150},
-		{Channel:1, Id:6, Time:-1, Angle:150},
+		{Channel:2, Id:5, Time:-1, Angle:150},
+		{Channel:2, Id:8, Time:-1, Angle:150},
+		{Channel:1, Id:2, Time:-1, Angle:150},
 	}
 
 	Servo.Action = action1
